7f1af6becd65039d153e70c53fc7c2a9c41a1fd9
Koji <dr.orange.jr@gmail.com>
2018-06-30 22:34:52 +0900

    tidy

--- run_analysis.R
+++ run_analysis.R
@@ -4,16 +4,87 @@
 ## 4. Appropriately labels the data set with descriptive variable names.
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+library(dplyr)
+library(tidyr)
+library(Hmisc) # capitalize()
+
+# Download the Data
+
 rawDataFileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-workingDataPath <- "./data"
-downloadedZipfilePath <- paste(workingDataPath, "downloaded_dataset.zip", sep = "/")
-rawDataPath <- paste(workingDataPath, "UCI HAR Dataset", sep ="/")
+workingDataPath <- file.path(".", "data")
+downloadedZipfilePath <- file.path(workingDataPath, "downloaded_dataset.zip")
+rawDataPath <- file.path(workingDataPath, "UCI HAR Dataset")
 if(!file.exists(workingDataPath)) {
         dir.create(workingDataPath)
-} else {print("SKIP: create working dir")}
+} else {
+        message("SKIP: create working dir")
+}
 if(!file.exists(downloadedZipfilePath)) {
         download.file(rawDataFileUrl, destfile = downloadedZipfilePath, method = "curl")
-} else {print("SKIP: download zip file")}
+} else {
+        message("SKIP: download zip file")
+}
 if(!file.exists(rawDataPath)) {
         unzip(zipfile = downloadedZipfilePath, exdir = workingDataPath)
-} else {print("SKIP: extract zip file")}
+} else {
+        message("SKIP: extract zip file")
+}
+
+# read data
+trainDataPath <- file.path(rawDataPath, "train")
+testDataPath <- file.path(rawDataPath, "test")
+
+subjectTrain <- tbl_df(read.table(file.path(trainDataPath, "subject_train.txt")))
+xTrain <- tbl_df(read.table(file.path(trainDataPath, "X_train.txt")))
+yTrain <- tbl_df(read.table(file.path(trainDataPath, "y_train.txt")))
+subjectTest <- tbl_df(read.table(file.path(testDataPath, "subject_test.txt")))
+xTest <- tbl_df(read.table(file.path(testDataPath, "X_test.txt")))
+yTest <- tbl_df(read.table(file.path(testDataPath, "y_test.txt")))
+featureNames <- read.table(file.path(rawDataPath, "features.txt"))
+activityNames <- read.table(file.path(rawDataPath, "activity_labels.txt"))
+
+# name
+names(subjectTrain) <- "SubjectId"
+names(subjectTest) <- "SubjectId"
+names(xTrain) <- featureNames$V2
+names(yTrain) <- "Activity"
+names(xTest) <- featureNames$V2
+names(yTest) <- "Activity"
+
+# free
+rm(featureNames)
+
+## 1. Merges the training and the test sets to create one data set.
+subjectData <- rbind(subjectTrain, subjectTest)
+xData <- rbind(xTrain, xTest)
+yData <- rbind(yTrain, yTest)
+data <- cbind(subjectData, yData, xData)
+
+# free
+rm(subjectTrain, subjectTest, xTrain, xTest, yTrain, yTest, subjectData, xData, yData)
+
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+data <- data[, grepl("SubjectId|Activity|mean\\(\\)|std\\(\\)", names(data))]
+
+## 3. Uses descriptive activity names to name the activities in the data set
+activityNames$V2 <- sapply(activityNames$V2, function(x) {paste(sapply(strsplit(tolower(x), "_"), capitalize), collapse=" ")}) 
+data$Activity <- factor(data$Activity, labels = activityNames$V2)
+
+## 4. Appropriately labels the data set with descriptive variable names.
+# capitalize each words
+names(data) <- sapply(names(data), function(x){
+        x <- gsub("-([XYZ])$", "\\1", x);
+        x <- gsub("^t", "Time", x);
+        x <- gsub("^f", "Frequency", x);
+        x <- gsub("Acc", "Acceleration", x);
+        x <- gsub("Gyro", "Gyroscope", x);
+        x <- gsub("Mag", "Magnitude", x);
+        x <- gsub("-mean\\(\\)", "Mean", x);
+        x <- gsub("-std\\(\\)", "StandardDeviation", x)
+})
+
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+meltedData <- melt(data, id = c("SubjectId", "Activity"))
+tidyData <- dcast(meltedData, SubjectId + Activity ~ variable, mean)
+
+write.table(tidyData, "tidy_data.csv", sep = ",")
